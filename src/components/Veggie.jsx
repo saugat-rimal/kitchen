b8bb33cc221a1a9b25786341b9a5a617024a3425
Saugat Rimal <saugatrimal60@gmail.com>
2023-01-02 08:22:34 +0545

    removed slick slider

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Slider from "react-slick";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const Veggie = () => {
   const [veggie, setVeggie] = useState([]);
